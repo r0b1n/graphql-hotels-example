f83abb03705dccfd1b5eed6927c658bd86e759f5
Roman Vyakhirev <roman.vyakhirev@gmail.com>
2016-05-31 18:04:41 +0300

    added images to cities and coverImage to country

--- graphql/schema.js
+++ graphql/schema.js
@@ -34,6 +34,22 @@ var node = relay.nodeDefinitions(
 
 // Declaring types here in one file because they have circular dependencies.
 
+var imageType = new types.GraphQLObjectType({
+  name: "Image",
+  description: "Image of city",
+  
+  fields: function () {
+    return {
+      id: relay.globalIdField('Image'),
+      url: {
+        type: types.GraphQLString,
+        description: "Image url. Relative to server address.",
+      },
+    }
+  },
+  interfaces: [node.nodeInterface],
+})
+
 // <editor-fold desc="countryType" defaultState="collapsed">
 var countryType = new types.GraphQLObjectType({
   name: 'Country',
@@ -44,6 +60,13 @@ var countryType = new types.GraphQLObjectType({
   fields: function () {
     return {
       id: relay.globalIdField('Country'),
+      _id: {
+        type: types.GraphQLString,
+        description: "Country id",
+        resolve: (data) => {
+          return data.id;
+        }
+      },
       name: {
         type: types.GraphQLString,
         description: 'The name of the country.',
@@ -51,6 +74,10 @@ var countryType = new types.GraphQLObjectType({
       slug: {
         type: types.GraphQLString,
         description: "Country slug",
+      },
+      coverImageUrl: {
+        type: types.GraphQLString,
+        description: "Cover image for country",
       },
       cities: {
         type: new types.GraphQLList(cityType),
@@ -100,6 +127,13 @@ var cityType = new types.GraphQLObjectType({
         resolve: function (city, params, source, fieldASTs) {
           return city.hotels.length;
         }
+      },
+      images: {
+        type: new types.GraphQLList(imageType),
+        description: "The list of Images of the City",
+        resolve: function (city, params, source, fieldASTs) {
+          return city.images.map((image, c) => {return {url: image, id: '' + city.id + c}})
+        },
       },
       country: {
         type: countryType,
@@ -163,72 +197,83 @@ console.log(cityConnection);
 
 
 // Declaring the heart of our GraphQL, the Schema.
+var userType =  new types.GraphQLObjectType({
+  name: 'ViewerQueryType',
+  description: "Root of all queries",
+
+  // all fields will be available from root query
+  // Like: `query { country (id: '10') { name }}`, etc.
+  fields: {
+
+    country: {
+      type: countryType,
+      args: {
+        // this is attributes for fetching node, they will should be passed in query.
+        // For example *id*: `query { country (id: '10') { name }}`
+        id: {
+          name: 'id',
+          type: new types.GraphQLNonNull(types.GraphQLString) // this attribute declared as is NonNull, so is is required.
+        }
+      },
+      // Important function which returns needed object, object will be converted to graphQL response.
+      // NOTE: we may return object with fill fields set, and GraphQL will deal with it and will trim not requested fields.
+      resolve: (root, params, source, fieldASTs) => {
+        // NOTE: `root` is a parent for current node, but because our node is inside root query `root` is undefined here
+
+        // NOTE: params here is hash with requested args (described above)
+        //  example: {id: '10'}
+
+        // simple function returns needed data.
+        return storage.getCountry(params);
+      }
+    },
+
+    countries: {
+      type: new types.GraphQLList(countryType),
+      description: "The list of Countries",
+      resolve: function (root, params, source, fieldASTs) {
+        // TODO: add sort/filter
+        return storage.getCountries();
+      }
+    },
+
+    city: {
+      type: cityType,
+      args: {
+        id: {
+          name: 'id',
+          type: new types.GraphQLNonNull(types.GraphQLString)
+        }
+      },
+      resolve: (root, params, source, fieldASTs) => {
+        return storage.getCity(params);
+      }
+    },
+
+    hotel: {
+      type: hotelType,
+      args: {
+        id: {
+          name: 'id',
+          type: new types.GraphQLNonNull(types.GraphQLString)
+        }
+      },
+      resolve: (root, params, source, fieldASTs) => {
+        return storage.getHotel(params);
+      }
+    },
+  }
+});
+
 module.exports = new types.GraphQLSchema({
   query: new types.GraphQLObjectType({
-    name: 'RootQueryType',
-    description: "Root of all queries",
-
-    // all fields will be available from root query
-    // Like: `query { country (id: '10') { name }}`, etc.
-    fields: {
-
-      country: {
-        type: countryType,
-        args: {
-          // this is attributes for fetching node, they will should be passed in query.
-          // For example *id*: `query { country (id: '10') { name }}`
-          id: {
-            name: 'id',
-            type: new types.GraphQLNonNull(types.GraphQLString) // this attribute declared as is NonNull, so is is required.
-          }
-        },
-        // Important function which returns needed object, object will be converted to graphQL response.
-        // NOTE: we may return object with fill fields set, and GraphQL will deal with it and will trim not requested fields.
-        resolve: (root, params, source, fieldASTs) => {
-          // NOTE: `root` is a parent for current node, but because our node is inside root query `root` is undefined here
-
-          // NOTE: params here is hash with requested args (described above)
-          //  example: {id: '10'}
-
-          // simple function returns needed data.
-          return storage.getCountry(params);
-        }
-      },
-
-      countries: {
-        type: new types.GraphQLList(countryType),
-        description: "The list of Countries",
-        resolve: function (root, params, source, fieldASTs) {
-          // TODO: add sort/filter
-          return storage.getCountries();
-        }
-      },
-
-      city: {
-        type: cityType,
-        args: {
-          id: {
-            name: 'id',
-            type: new types.GraphQLNonNull(types.GraphQLString)
-          }
-        },
-        resolve: (root, params, source, fieldASTs) => {
-          return storage.getCity(params);
-        }
-      },
-
-      hotel: {
-        type: hotelType,
-        args: {
-          id: {
-            name: 'id',
-            type: new types.GraphQLNonNull(types.GraphQLString)
-          }
-        },
-        resolve: (root, params, source, fieldASTs) => {
-          return storage.getHotel(params);
-        }
-      },
-    }
-  })
+  name: 'Query',
+  fields: () => ({
+    // Add your own root fields here
+    viewer: {
+      type: userType,
+      resolve: () => {return {}},
+    },
+  }),
+}),
 });
